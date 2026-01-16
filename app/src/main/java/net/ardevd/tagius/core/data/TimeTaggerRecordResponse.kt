f2a2261c2ca50a8f8cdbdae0ce6e11b0d5a31503
ardevd <ardevd@users.noreply.github.com>
2026-01-16 23:36:15 +0100

    refactor: enabled R8 minify, added required R8 rules and annotations (#15)

--- app/src/main/java/net/ardevd/tagius/core/data/TimeTaggerRecordResponse.kt
+++ app/src/main/java/net/ardevd/tagius/core/data/TimeTaggerRecordResponse.kt
@@ -1,5 +1,8 @@
 package net.ardevd.tagius.core.data
 
+import com.google.gson.annotations.SerializedName
+
 data class TimeTaggerRecordResponse(
+    @SerializedName("records")
     val records: List<TimeTaggerRecord>
 )
