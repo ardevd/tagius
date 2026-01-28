0f1e661aa197df3c8a6b144e4a192184806604dc
ardevd <ardevd@protonmail.com>
2026-01-28 19:52:48 +0100

    chore: version bump to v0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.ardevd.tagius"
         minSdk = 34
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.7.0"
+        versionCode = 5
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
