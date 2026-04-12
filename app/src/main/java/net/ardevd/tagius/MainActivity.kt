9655034d99e8e41f9fe4aa9a170f94cdad296b49
ardevd <ardevd@protonmail.com>
2026-04-12 21:29:02 +0200

    feat: added dynamic color theme support

--- app/src/main/java/net/ardevd/tagius/MainActivity.kt
+++ app/src/main/java/net/ardevd/tagius/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.color.DynamicColors
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import net.ardevd.tagius.core.data.TokenManager
@@ -70,13 +71,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val tokenManager = TokenManager(this)
+        if (tokenManager.isDynamicColorsEnabledBlocking()) {
+            DynamicColors.applyToActivityIfAvailable(this)
+        }
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.topAppBar)
-        val tokenManager = TokenManager(this)
         lifecycleScope.launch {
             val token = tokenManager.authTokenFlow.first()
             if (savedInstanceState == null) {
